7d8bcc5086d6df3b08dfb0c0bb9a5eb5beb4836f
DevExpressExampleBot <fakeEmail>
2021-09-02 17:05:22 +0300

    autogenerate VB

--- VB/HowToSaveLayout/MainWindow.xaml.vb
+++ VB/HowToSaveLayout/MainWindow.xaml.vb
@@ -1,5 +1,4 @@
-﻿Imports Microsoft.VisualBasic
-Imports System.Data
+﻿Imports System.Data
 Imports System.Data.OleDb
 Imports System.Windows
 Imports DevExpress.Xpf.PivotGrid
@@ -8,22 +7,20 @@ Imports HowToBindToMDB.NwindDataSetTableAdapters
 Namespace HowToBindToMDB
 	Partial Public Class MainWindow
 		Inherits Window
-		Private salesPersonDataTable As New NwindDataSet.SalesPersonDataTable()
-		Private salesPersonDataAdapter As New SalesPersonTableAdapter()
 
+'INSTANT VB NOTE: The variable salesPersonDataTable was renamed since it may cause conflicts with calls to static members of the user-defined type with this name:
+		Private salesPersonDataTable_Conflict As New NwindDataSet.SalesPersonDataTable()
+		Private salesPersonDataAdapter As New SalesPersonTableAdapter()
 		Public Sub New()
 			InitializeComponent()
-			pivotGridControl1.DataSource = salesPersonDataTable
+			pivotGridControl1.DataSource = salesPersonDataTable_Conflict
 		End Sub
-
 		Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			salesPersonDataAdapter.Fill(salesPersonDataTable)
+			salesPersonDataAdapter.Fill(salesPersonDataTable_Conflict)
 		End Sub
-
 		Private Sub buttonSave_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
 			pivotGridControl1.SaveLayoutToXml("layout.xml")
 		End Sub
-
 		Private Sub buttonLoad_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
 			pivotGridControl1.RestoreLayoutFromXml("layout.xml")
 		End Sub
