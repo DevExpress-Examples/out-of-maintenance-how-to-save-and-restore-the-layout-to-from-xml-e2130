38b9823dafeed1b4d14de9076638ced1ef02b6a2
DevExpressExampleBot <fakeEmail>
2022-05-24 10:07:17 +0400

    Source auto update [skip ci]

--- VB/HowToSaveLayout/MainWindow.xaml.vb
+++ VB/HowToSaveLayout/MainWindow.xaml.vb
@@ -1,31 +1,30 @@
-﻿Imports Microsoft.VisualBasic
-Imports System.Data
-Imports System.Data.OleDb
 Imports System.Windows
-Imports DevExpress.Xpf.PivotGrid
 Imports HowToBindToMDB.NwindDataSetTableAdapters
 
 Namespace HowToBindToMDB
-	Partial Public Class MainWindow
-		Inherits Window
-		Private salesPersonDataTable As New NwindDataSet.SalesPersonDataTable()
-		Private salesPersonDataAdapter As New SalesPersonTableAdapter()
 
-		Public Sub New()
-			InitializeComponent()
-			pivotGridControl1.DataSource = salesPersonDataTable
-		End Sub
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			salesPersonDataAdapter.Fill(salesPersonDataTable)
-		End Sub
+        Private salesPersonDataTable As NwindDataSet.SalesPersonDataTable = New NwindDataSet.SalesPersonDataTable()
 
-		Private Sub buttonSave_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			pivotGridControl1.SaveLayoutToXml("layout.xml")
-		End Sub
+        Private salesPersonDataAdapter As SalesPersonTableAdapter = New SalesPersonTableAdapter()
 
-		Private Sub buttonLoad_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			pivotGridControl1.RestoreLayoutFromXml("layout.xml")
-		End Sub
-	End Class
+        Public Sub New()
+            Me.InitializeComponent()
+            Me.pivotGridControl1.DataSource = salesPersonDataTable
+        End Sub
+
+        Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            salesPersonDataAdapter.Fill(salesPersonDataTable)
+        End Sub
+
+        Private Sub buttonSave_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Me.pivotGridControl1.SaveLayoutToXml("layout.xml")
+        End Sub
+
+        Private Sub buttonLoad_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Me.pivotGridControl1.RestoreLayoutFromXml("layout.xml")
+        End Sub
+    End Class
 End Namespace
